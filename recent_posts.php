dd95fe46f8a92ea271f8aa248c8774b0948e66bf
Wang Yen-Hsiang <weric3690606@gmail.com>
2020-09-11 02:05:26 +0800

    Update recent_posts.php

--- recent_posts.php
+++ recent_posts.php
@@ -1,7 +1,6 @@
 <?php
 $HyperlinkArray = array(
-  array("href="/2019/04/29/article-title/",article title),
-  array("href="/2019/04/29/hello-world/",Hello World),
-  array("href="/2019/04/29/hello-world/",testtt))
-  );
+  array("href="/2019/04/29/article-title/","article title"),
+  array("href="/2019/04/29/hello-world/","Hello World"),
+  array("href="/2019/04/29/hello-world/","testtt");
 ?>
