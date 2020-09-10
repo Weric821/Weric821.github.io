a2bcc53bd56d59a8a8f8616db1103c0d5a282723
Wang Yen-Hsiang <weric3690606@gmail.com>
2020-09-11 02:03:41 +0800

    Update and rename recent_posts.html to recent_posts.php

---
+++ recent_posts.php
@@ -0,0 +1,7 @@
+<?php
+$HyperlinkArray = array(
+  array("href="/2019/04/29/article-title/",article title),
+  array("href="/2019/04/29/hello-world/",Hello World),
+  array("href="/2019/04/29/hello-world/",testtt))
+  );
+?>
